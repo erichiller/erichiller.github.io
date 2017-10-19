0d0c9438ba2b6aa7c8a5a5a7b7a6c16bec8c1aee
Eric Hiller <ehiller@riotgames.com>
2017-10-19 12:57:22 -0700

    update to css rel-link

--- gandi_update.ps1
+++ gandi_update.ps1
@@ -1,6 +1,22 @@
 
-$APIKEY = "API KEY FROM GANDI"
-$DOMAIN = "hiller.pro"
+
+param(
+	[Parameter(Mandatory=$true, HelpMessage="API Key from Gandi for Live DNS")]
+	[String]
+	$APIKEY,
+	
+	[Parameter(Mandatory=$true, HelpMessage="SHA String from certbot")]
+	[String]
+	$CHALLENGE_VALUE,
+
+	[Parameter(Mandatory=$false)]
+	[String]
+	$DOMAIN = "hiller.pro",
+
+	[Parameter(Mandatory=$false, HelpMessage="do not add domain portion of string, Gandi does this for us")]
+	[String]
+	$RECORD_NAME = "_acme_challenge"
+)
 
 # get all records on domain
 # curl -H "X-Api-Key: $APIKEY" https://dns.api.gandi.net/api/v5/domains/hiller.pro/records | ConvertFrom-Json
@@ -9,9 +25,7 @@ $DOMAIN = "hiller.pro"
 # use the curl flag --trace-ascii <logfile> for tracing issues
 # curl -v is good for headers only
 
-$CHALLENGE_VALUE = "value" # SHA String from running 
 # certbot certonly --manual --preferred-challenges dns -d hiller.pro --agree-tos --redirect --hsts --uir
-$RECORD_NAME = "_acme-challenge" # DO NOT ADD THE MAIN DOMAIN, GANDI DOES THIS FOR US!
 @{
 	"rrset_name"	= "$RECORD_NAME"
 	"rrset_type"	= "TXT"
